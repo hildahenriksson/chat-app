d851d2ac0c7882655b2b73dd687e32dae32f255b
Hilda Henriksson <hilda.henriksson@hotmail.com>
2023-09-03 08:06:52 +0200

    icons

--- navigators/BottomTabNavigator.js
+++ navigators/BottomTabNavigator.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Camera from '../pages/Camera';
 import Profile from '../pages/Profile';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,8 +11,20 @@ export default function BottomTagNavigator() {
         screenOptions={{
         headerShown: false
     }}>
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Camera" component={Camera} />
+      <Tab.Screen name="Profile" component={Profile} 
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: () => (
+            <Ionicons name="person-circle" size={34} color="black" />
+          ),
+        }}/>
+      <Tab.Screen name="Camera" component={Camera} 
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: () => (
+            <Entypo name="camera" size={30} color="black" />
+          ),
+        }}/>
     </Tab.Navigator>
   );
 }
